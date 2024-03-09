22836fb6b186715e9b80a799299b3b0a088537db
pavanydg <pavanyermunja@gmail.com>
2024-03-09 15:17:02 +0530

    added sigin users

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -4,12 +4,18 @@ import  Button  from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { Subheading } from "../components/Subheading"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 
 export const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleSignIn = async () => {
     try {
-      const { user, error } = await supabase.auth.signIn({
+      const { user, error } = await supabase.auth.signInWithPassword({
         email,
         password,
       });
@@ -30,8 +36,8 @@ export const Signin = () => {
       <div className="rounded-lg text-center bg-white p-2 px-4 h-max w-80">
         <Heading label={'Sign In'}/>
         <Subheading label={'Enter your credentials to access your account'}/>
-        <InputBox label={'Email'} placeholder={"xyz@gmail.com"}/>
-        <InputBox label={'Password'} placeholder={"123456"}/>
+        <InputBox label={'Email'} placeholder={"xyz@gmail.com"} onChange={(e) => setEmail(e.target.value)}/>
+        <InputBox label={'Password'} placeholder={"123456"} onChange={(e) => setPassword(e.target.value)}/>
         <div className='hover:cursor-pointer
                 bg-purple-500 rounded-md p-1.5 
             ' onClick={handleSignIn}>SignIn</div>
